40d4baa87df89622e25c31720e321765353ff0c8
katayama <kata.127.yama@gmail.com>
2025-03-29 21:02:19 +0900

    他メソッドを実装

--- src/lib.rs
+++ src/lib.rs
@@ -15,14 +15,36 @@ mod library {
         }
     }
     mod bookshelf {
+        use super::book::Book;
+
         struct Bookshelf {
             // books: Vec<crate::library::book::Book>,
             books: Vec<super::book::Book>,
         }
 
         impl Bookshelf {
-            fn new() -> Self {
+            pub fn new() -> Self {
                 Self { books: Vec::new() }
+            }
+
+            // 本を追加するメソッド
+            pub fn add_book(&mut self, book: Book) {
+                self.books.push(book);
+            }
+
+            // タイトルで本を検索するメソッド
+            pub fn search_books(&self, title_query: &str) -> &Vec<Book> {
+                todo!("Implementend");
+            }
+
+            // 本を本棚から取り出すメソッド
+            pub fn remobe_book(&mut self, book: &Book) -> Option<Book> {
+                todo!("Implemented");
+            }
+
+            // 本棚の本をすべて取り出すメソッド
+            pub fn take_all_books(&mut self) -> Vec<Book> {
+                todo!("Implemented");
             }
         }
     }

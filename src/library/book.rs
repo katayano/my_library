f9b288be54adbcea8c4dfca956d6d1178e7c50b5
katayama <kata.127.yama@gmail.com>
2025-03-29 23:40:40 +0900

    あいまい検索できるように修正

--- src/library/book.rs
+++ src/library/book.rs
@@ -1,10 +1,11 @@
+#[derive(Debug)]
 pub struct Book {
     pub title: String,
     author: String,
 }
 
 impl Book {
-    fn new(title: &str, author: &str) -> Self {
+    pub fn new(title: &str, author: &str) -> Self {
         Self {
             title: title.to_string(),
             author: author.to_string(),
--- src/library/bookshelf.rs
+++ src/library/bookshelf.rs
@@ -1,13 +1,19 @@
 use super::book::Book;
+use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
 
 pub struct Bookshelf {
     // books: Vec<crate::library::book::Book>,
     books: Vec<super::book::Book>,
+    matcher: SkimMatcherV2,
 }
 
 impl Bookshelf {
     pub fn new() -> Self {
-        Self { books: Vec::new() }
+        let matcher = SkimMatcherV2::default();
+        Self {
+            books: Vec::new(),
+            matcher,
+        }
     }
 
     // 本を追加するメソッド
@@ -31,6 +37,14 @@ impl Bookshelf {
             .collect()
     }
 
+    // タイトルで本を検索するメソッド
+    pub fn search_books(&self, title_query: &str) -> Vec<&Book> {
+        self.books
+            .iter()
+            .filter(|book| self.matcher.fuzzy_match(&book.title, title_query).is_some())
+            .collect()
+    }
+
     // 本を本棚から取り出すメソッド
     pub fn remobe_book(&mut self, book: &Book) -> Option<Book> {
         todo!("Implemented");
@@ -39,5 +53,24 @@ impl Bookshelf {
     // 本棚の本をすべて取り出すメソッド
     pub fn take_all_books(&mut self) -> Vec<Book> {
         todo!("Implemented");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Book;
+    use super::Bookshelf;
+
+    #[test]
+    fn test_bookshelf() {
+        let mut shelf = Bookshelf::new();
+        let book1 = Book::new("すごいぞChatGPT!AIを使って学ぼうRust!", "山田太郎");
+        let book2 = Book::new("Pythonプログラミング入門", "山田花子");
+        // 本を追加
+        shelf.add_book(book1);
+        shelf.add_book(book2);
+        // 本を検索
+        let found_books = shelf.search_books("chatgpt");
+        println!("{:?}", found_books);
     }
 }

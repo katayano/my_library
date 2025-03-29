a2bf64b74b494f96d6a148dfaaeffc5b002a8375
katayama <kata.127.yama@gmail.com>
2025-03-29 19:14:48 +0900

    book, bookshelfモジュールを作成

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+mod library {
+    mod book {
+        pub struct Book {
+            title: String,
+            author: String,
+        }
+
+        impl Book {
+            fn new(title: &str, author: &str) -> Self {
+                Self {
+                    title: title.to_string(),
+                    author: author.to_string(),
+                }
+            }
+        }
+    }
+    mod bookshelf {
+        struct Bookshelf {
+            // books: Vec<crate::library::book::Book>,
+            books: Vec<super::book::Book>,
+        }
+
+        impl Bookshelf {
+            fn new() -> Self {
+                Self { books: Vec::new() }
+            }
+        }
+    }
+}
